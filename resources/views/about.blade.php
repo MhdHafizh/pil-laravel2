7063ce16873ea78feb66270f89065cbdeb9b7d4f
unknown <mh8716185@gmail.com>
2023-11-16 17:57:06 +0700

    Push pertama saya untuk project laravel kedalam github

---
+++ resources/views/about.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>About</title>
+</head>
+<body>
+<h1>Ini adalah halaman About</h1>    
+<ul>
+    <li><a href= "/"> Home</a></li>
+    <li><a href="/profile">profile</a></li>
+    <li><a href="/mahasiswa">Mahasiswa</a></li>
+    <li><a href="/about">About</a></li>
+</ul>
+
+
+<p>Nama saya adalah {{ $nama; }}</p>
+
+</body>
+</html>
---
+++ resources/views/mahasiswa.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>mahasiswa</title>
+</head>
+<body>
+<h1>Ini adalah halaman mahasiswa</h1> 
+<ul>
+    <li><a href= "/"> Home</a></li>
+    <li><a href="/profile">profile</a></li>
+    <li><a href="/mahasiswa">Mahasiswa</a></li>
+    <li><a href="/about">About</a></li>
+</ul>
+</body>
+</html>
