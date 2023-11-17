2f839337a3deda0fa5ad6e4a3a6bc451faa992ac
unknown <mh8716185@gmail.com>
2023-11-17 16:32:54 +0700

    Menggunakan bootstrap 5

--- routes/web.php
+++ routes/web.php
@@ -8,21 +8,34 @@ use Illuminate\Support\Facades\Route;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
 |
 */
 
 Route::get('/', function () {
-    return view('home');
+    return view('index');
+   
 });
-Route::get('about/', function () {
-    $nama = 'jaka sembung';
-    return view('about')->with('nama', $nama);
+
+Route::get('about', function () {
+    //$nama = 'Muhammad Hafizh Azman Matondang';
+    //$prodi = 'Teknologi Informasi';
+    //return view('about')->with('nama', $nama);
+    return view('about');
+   
 });
-Route::get('mahasiswa/', function () {
-    return view('mahasiswa');
+
+Route::get('profile', function () {
+    $nama = 'Muhammad Hafizh Azman Matondang';
+    return view('profile')->with('nama', $nama);
+   
 });
-Route::get('profile/', function () {
-    return view('profile');
+
+Route::get('mahasiswa', function () {
+    $npm = [123,124,125,126];
+    $nama = ['Muhammad','Hafizh','Azman','Matondang'];
+    $jumlah = count($npm);
+    return view('mahasiswa',compact('npm','jumlah','nama'));  
+    
 });
